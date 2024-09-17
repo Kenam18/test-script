a883a25ffcddba12034897f6ade2eef785e2ca7b
Kenam Verma <kenam.verma@gmail.com>
2024-09-17 12:46:01 +0530

    add docker

--- scripts/pr-level-check.sh
+++ scripts/pr-level-check.sh
@@ -11,5 +11,7 @@ sleep 10
 
 # Run Sitespeed
 # sitespeed.io $URL -b $BROWSER -n $NUM_RUNS
-podman run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:latest http://host.docker.internal:3000
+# podman run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:latest http://host.docker.internal:3000
+docker run --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:latest http://host.docker.internal:3000
+
 # docker --version
